8b0eaadf5cdfda11fc9c9a66835b2015b1006524
Alfred Neumayer <dev.beidl@gmail.com>
2022-10-18 00:48:40 +0200

    ui/sdl2: Pocket VMs branding in the title bar

--- ui/sdl2.c
+++ ui/sdl2.c
@@ -156,12 +156,12 @@ static void sdl_update_caption(struct sdl2_console *scon)
     }
 
     if (qemu_name) {
-        snprintf(win_title, sizeof(win_title), "QEMU (%s-%d)%s", qemu_name,
+        snprintf(win_title, sizeof(win_title), "Pocket VMs with QEMU (%s-%d)%s", qemu_name,
                  scon->idx, status);
-        snprintf(icon_title, sizeof(icon_title), "QEMU (%s)", qemu_name);
+        snprintf(icon_title, sizeof(icon_title), "Pocket VMs with QEMU (%s)", qemu_name);
     } else {
-        snprintf(win_title, sizeof(win_title), "QEMU%s", status);
-        snprintf(icon_title, sizeof(icon_title), "QEMU");
+        snprintf(win_title, sizeof(win_title), "Pocket VMs with QEMU%s", status);
+        snprintf(icon_title, sizeof(icon_title), "Pocket VMs with QEMU");
     }
 
     if (scon->real_window) {
